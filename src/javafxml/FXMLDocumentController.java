d8c43c9999105659b4e8d23917b0f126301817cb
russo.salvatore <russo.salvatore@TAVE-PC27.OSDBSESTO.local>
2021-05-04 11:53:40 +0200

    finish

--- src/javafxml/FXMLDocumentController.java
+++ src/javafxml/FXMLDocumentController.java
@@ -25,7 +25,7 @@ import javafx.scene.shape.Rectangle;
  * @author russo.salvatore
  */
 public class FXMLDocumentController implements Initializable {
-    private static int pos=0;
+    private int pos=0;
     @FXML
     private Label label;
     @FXML
@@ -47,39 +47,54 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Button indietro=new Button();
     private Rectangle[]a={r1,r2,r3,r4,r5,r6,r7};
+    private Rectangle m=new Rectangle();
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
+        System.out.println(a.length);
         label.setText("Hello World!");
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        avanti.setOnAction(new EventHandler<ActionEvent>(){
-            @Override
-            public void handle(ActionEvent event) {
-             //  this.avanti();
-            }
-            
-        });
+        a[0]=r1;
+        a[1]=r2;
+        a[2]=r3;
+        a[3]=r4;
+        a[4]=r5;
+        a[5]=r6;
+        a[6]=r7;
+                 
     }    
 
+
+
     @FXML
-    private void avanti(ActionEvent event) {
+    private void avanti(MouseEvent event) {
         if(pos<a.length-1){
-            a[pos].setStroke(Color.WHITE);
+            a[pos].setFill(Color.WHITE);
+            a[pos].setStroke(Color.BLACK);
+            a[pos+1].setFill(Color.RED);
             a[pos+1].setStroke(Color.RED);
             pos++;
         }
     }
 
+
     @FXML
-    private void indietro(ActionEvent event) {
+    private void indietro(MouseEvent event) {
          if(pos>0){
-            a[pos].setStroke(Color.WHITE);
-            a[pos-1].setStroke(Color.RED);
+            a[pos].setFill(Color.WHITE);
+             a[pos].setStroke(Color.BLACK);
+             a[pos-1].setStroke(Color.RED);
+            a[pos-1].setFill(Color.RED);
             pos--;
         }
     }
+
+
+
+ 
+    
+
 }
 
    
